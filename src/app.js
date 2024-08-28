70bcc78ddff9b47729fe3e7030a1098f8cc92384
Jose M Cuervo <josmansoft24@gmail.com>
2024-08-28 16:25:11 -0500

    first commit

--- src/app.js
+++ src/app.js
@@ -5,11 +5,6 @@ import {PORT} from './config.js'
 
 const app = express()
 
-app.get('/', async(req, res)=>{
-    const rows = await pool.query("select * from preguntas")
-    res.json(rows)
-})
-
 app.get('/ping', (req, res)=>{
     const [result] = pool.query("select 'hola mundo' as result");
     res.json(result[0])
