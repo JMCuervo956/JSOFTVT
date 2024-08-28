1e91c7953774aa6bec4fdc6c4252466d7fd8615a
Jose M Cuervo <josmansoft24@gmail.com>
2024-08-28 18:21:45 -0500

    first commit

--- src/app.js
+++ src/app.js
@@ -4,11 +4,21 @@ import {pool} from './db.js'
 import {PORT} from './config.js'
 
 const app = express()
-
+/*
 app.get('/', async(req, res)=>{
     const rows = await pool.query("select * from preguntas")
     res.json(rows)
 })
+*/
+app.get('/', async (req, res) => {
+    try {
+      const result = await pool.query("SELECT * FROM preguntas");
+      res.json(result.rows);
+    } catch (err) {
+      console.error('Error executing query', err.stack);
+      res.status(500).send('Error retrieving data');
+    }
+  });
 
 app.get('/ping', async(req, res)=>{
     const [result] = await pool.query("select 'hola mundo' as result");
@@ -20,5 +30,10 @@ app.get('/create', async(req, res)=>{
     res.json(result)
 });
 
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+  });
+/*
  app.listen(PORT)
  console.log('Server en port', PORT)
+ */
