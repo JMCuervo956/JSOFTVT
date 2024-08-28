36458d81545a2e9c98fb4473da07703b8a61d790
Jose M Cuervo <josmansoft24@gmail.com>
2024-08-28 17:45:44 -0500

    first commit

--- src/app.js
+++ src/app.js
@@ -5,10 +5,16 @@ import {PORT} from './config.js'
 
 const app = express()
 
+app.get('/', (req, res)=>{
+        res.send('Hola aqui estamos VALIDAR PROCESOS')
+    }) 
+    
+/*
 app.get('/', async(req, res)=>{
     const rows = await pool.query("select * from preguntas")
     res.json(result.rows)
 })
+*/
 
 app.listen(PORT)
 console.log('Server en port', PORT)
